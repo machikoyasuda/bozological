18a61eaf43e50de24e4f42539757ea0cb14cdeb2
Machiko Yasuda <machikoyasuda@gmail.com>
2013-07-15 10:07:22 -0700

    Add postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bozological::Application.config.secret_key_base = '9849d7f6dfcb7bc687650e56033435c75f7cdd2ad8a122a004b8a97a04e285f993d59cc6dcf2a6014d1590f6b5a610be4ed8f8b2c35a7e98e07536b17ca00f6f'
