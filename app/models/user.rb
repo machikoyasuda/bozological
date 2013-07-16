2dca7ca61f670ee2904c5f07ac84babb023ea617
Machiko Yasuda <machikoyasuda@gmail.com>
2013-07-16 14:52:34 -0700

    Initialize Fabricator and integrate fabricated users into user_spec.rb test.

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ActiveRecord::Base
+  before_save :encrypt_password
+
+  attr_accessor :password, :password_confirmation
+
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+
+    if user
+      fish = BCrypt::Engine.hash_secret(password, user.salt)
+      if user.fish == fish
+        return user
+      end
+    end
+    nil
+
+  end
+
+  private
+
+  def encrypt_password
+    self.salt = BCrypt::Engine.generate_salt
+    self.fish = BCrypt::Engine.hash_secret(password, self.salt)
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,26 @@
 require 'spec_helper'
 
 describe User do
-  before(:each) do
-    @password = "123"
-    @email = "joe@chicago.com"
-
-    @user = User.create(
-      email: @email,
-      password: @password,
-      password_confirmation: @password
-    )
-  end
+  subject(:user) { Fabricate(:user) }
 
   it "encrypts a password" do
-    fish = BCrypt::Engine.hash_secret(@password, @user.salt)
+    fish = BCrypt::Engine.hash_secret("123", subject.salt)
 
-    expect(@user.salt).not_to be_nil
-    expect(@user.fish).to eq(fish)
+    puts subject.email
+    expect(subject.salt).not_to be_nil
+    expect(subject.fish).to eq(fish)
   end
 
   it "authenticates a user" do
-    authuser = User.authenticate(@email, @password)
-    unauthuser = User.authenticate(@email, "")
+    authuser = User.authenticate(subject.email, "123")
+    unauthuser = User.authenticate(subject.email, "")
 
-    expect(authuser).to eq(@user)
+    puts subject.email
+    expect(authuser).to eq(subject)
     expect(unauthuser).to be_nil
   end
 end
+
+# Instead of creating a new user every time,
+# We will create test users from the start
+# using Ffaker and Fabrication
