7ae5f6e07e283cadb08fee356984d33be6468be9
Machiko Yasuda <machikoyasuda@gmail.com>
2013-07-16 16:24:12 -0700

    Add sessions_controller test for POST # create authentication and password code.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,26 +1,34 @@
 require 'spec_helper'
 
 describe SessionsController do
+  let!(:email) { "sam@stone.com" }
+  let!(:pw) { "123xyz" }
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
+  subject!(:user) { Fabricate(
+    :user,
+    email: email,
+    password: pw,
+    password_confirmation: pw
+  )}
+
+  describe "POST # create" do
+    it "logs an authenticated user in and redirects to site#index" do
+      post("create", email: email, password: pw)
+
+      expect(response).to redirect_to(root_url)
+    end
+
+    it "redisplays the login form for authentication failures" do
+      post("create", email: email, password: "...")
+
+      expect(response).to render_template("new")
+    end
+
+    it "sets code and expires_at for a password reset request" do
+      post("create", email: email, password: "")
+
+      expect(user.code).not_to be_nil
+      expect(user.expires_at).not_to be_nil
     end
   end
-
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'destory'" do
-    it "returns http success" do
-      get 'destory'
-      response.should be_success
-    end
-  end
-
 end
