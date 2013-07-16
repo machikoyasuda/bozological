89760b7b168271bdc7c372fb415ed4eb312c62e7
Machiko Yasuda <machikoyasuda@gmail.com>
2013-07-16 14:57:17 -0700

    Revise spelling error in controllers.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,21 @@
 class SessionsController < ApplicationController
-  def new
+  before_action :is_authenticated, only: [:new]
+  def new                               # looks for new.html.erb and displays it
+    #render :json => session             # return sessions hash as a JSON
   end
 
-  def create
-  	redirect_to root_url
+  def create                            # authenticate new log-in session user
+    user = User.authenticate(params[:email], params[:password])
+
+    if user
+        session[:user_id] = user.id
+        redirect_to root_url
+    else
+        render :new
+    end
   end
 
-  def destory
+  def destroy                           # end current log-in session
   	redirect_to login_url
   end
 end
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,8 @@
 class SiteController < ApplicationController
+  before_action :is_authenticated, only: [:index]
+
   def index
+    render :json => params
   end
 
   def privacy
